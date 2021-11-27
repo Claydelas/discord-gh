e523c565e3b1aa08097cc83c9a25a94f83c74e38
Martin Valchev <mv1u18@soton.ac.uk>
2021-11-27 18:03:48 +0200

    chore: replace placeholder content from Create Next App, configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,jsx,ts,tsx}', './utils/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
